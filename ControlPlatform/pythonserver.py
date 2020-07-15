09f30c51c2b32bfaf76ae0f0a20bb1e115c142e4
liroding <liroding@126.com>
2020-07-15 21:02:11 +0800

    update code

--- ControlPlatform/pythonserver.py
+++ ControlPlatform/pythonserver.py
@@ -12,7 +12,7 @@ from SocketServer import ThreadingMixIn
 import cgi
 import json
 from configobj import ConfigObj
-
+import random
 
 PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
 
@@ -28,16 +28,26 @@ class HandleCfgFile():
       fd = open(filename,'r')
       return fd
 
-  def tojsondata(self,filename):
+        
+  def tojsondata(self,filename,*keyarg):
+        print keyarg 
+        print (type(keyarg))
+
         _list = []
         config = ConfigObj(filename,encoding='UTF8')
-        print(config['ARG'])
-        _list.append(config['ARG'])
-        _list.append(config['CASE'])
+        
+        for item in keyarg:
+            _list.append(config[item])
         print("[LIRO-DEBUG] liststring = %s " % _list)
         jsonArr = json.dumps(_list, ensure_ascii=False)
         print("[LIRO-DEBUG] jsonstring = %s " % jsonArr)
         return jsonArr
+        
+        
+        
+        
+        
+        
         '''
         _fd = self.openfile(filename)
         _list = []
@@ -56,27 +66,23 @@ class HandleCfgFile():
         return jsonArr
         '''
   def writebackcfgini(self,filename,newdata):  #newdata is list struct
-	config = ConfigObj(filename,encoding='UTF8')
+        config = ConfigObj(filename,encoding='UTF8')
+        for i in range(len(newdata)):
+                print(newdata[i])
+                for k,v in newdata[i].items():
+                    for data in config.items():
+                        _ITEM = data[0]    # [ ]
+                        _arg  = data[1]
+                        #print(_item)
+                        #print(_arg)
+                        for item in _arg:
+                            if k == item:
+                                print(k)
+                                print(_ITEM)
+                                print(config[_ITEM][k])
+                                config[_ITEM][k] = v
+                                config.write()
 
-	for i in range(len(newdata)):
-		print(newdata[i])
-		for k,v in newdata[i].items():
-			for data in config.items():
-				_ITEM = data[0]    # [ ]
-				_arg  = data[1]
-				#print(_item)
-				#print(_arg)
-				for item in _arg:
-					if k == item:
-						print(k)
-						print(_ITEM)
-						print(config[_ITEM][k])
-						config[_ITEM][k] = v
-						config.write()
-	
-
-					
-		
 
 #自定义处理程序，用于处理HTTP请求
 class TestHTTPHandler(BaseHTTPRequestHandler):
@@ -168,90 +174,49 @@ class TestHTTPHandler(BaseHTTPRequestHandler):
        self.end_headers()  
 
        
-       if path == '/hifcfg.ini':
-            print 'get the AppCfg.txt'
-            print (PROJECT_ROOT)
-
-            ''' 
-            f = open(PROJECT_ROOT+ '/app.ini',"rb")
-            for line in f.readlines():            # 输出文件内容
-                
-                self.wfile.write(line)
-                
-            f.close()
-            
-	    '''
-            jsonStr = handleclass.tojsondata(PROJECT_ROOT+ '/hifcfg.ini')
-            self.wfile.write(jsonStr)
-            
-            return 0
-
+       print( path )
 
        if path == '/cosim/cfgini/app.ini':
             print 'get the app.ini'
             print (PROJECT_ROOT)
-            jsonStr = handleclass.tojsondata(PROJECT_ROOT+ '/cosim/cfgini/app.ini')
+            jsonStr = handleclass.tojsondata(PROJECT_ROOT+ '/cosim/cfgini/app.ini',*{'ARG','CASE'})
             self.wfile.write(jsonStr)
 
-	    return 0
+            return 0
 
 
 
        if path == '/cosim/cfgini/qemu.ini':
             print 'get the qemu.ini'
             print (PROJECT_ROOT)
-            jsonStr = handleclass.tojsondata(PROJECT_ROOT+ '/cosim/cfgini/qemu.ini')
+            jsonStr = handleclass.tojsondata(PROJECT_ROOT+ '/cosim/cfgini/qemu.ini',*{'ARG','CASE'})
             self.wfile.write(jsonStr)
 
-	    return 0
+            return 0
 
 
 
        if path == '/cosim/cfgini/app.ini/update':    #app cfg submit
             print 'submit new cfg data to app.ini'
-	    jsonobj = json.loads(content)  #string to jsonobj
-	    #print(type(jsonobj))
-	    #print(jsonobj)
+            jsonobj = json.loads(content)  #string to jsonobj
+            #print(type(jsonobj))
+            #print(jsonobj)
 
-	    handleclass.writebackcfgini(PROJECT_ROOT+ '/cosim/cfgini/app.ini',jsonobj)
-            '''
-	    self.handleclass =  HandleCfgFile()
-            jsonStr = handleclass.tojsondata(PROJECT_ROOT+ '/hifcfg.ini')
-            self.wfile.write(jsonStr)
-            '''
+            handleclass.writebackcfgini(PROJECT_ROOT+ '/cosim/cfgini/app.ini',jsonobj)
             return 0         
 
 
        if path == '/cosim/cfgini/qemu.ini/update':    #qemu cfg submit
             print 'submit new cfg data to qemu.ini'
-	    jsonobj = json.loads(content)  #string to jsonobj
+            jsonobj = json.loads(content)  #string to jsonobj
 
-	    handleclass.writebackcfgini(PROJECT_ROOT+ '/cosim/cfgini/qemu.ini',jsonobj)
-            '''
-	    self.handleclass =  HandleCfgFile()
-            jsonStr = handleclass.tojsondata(PROJECT_ROOT+ '/hifcfg.ini')
-            self.wfile.write(jsonStr)
-            '''
+            handleclass.writebackcfgini(PROJECT_ROOT+ '/cosim/cfgini/qemu.ini',jsonobj)
             return 0         
 
 
 
 
 
-       if path == '/id=1':    #app cfg submit
-            print 'submit new cfg data to hifcfg.ini'
-	    jsonobj = json.loads(content)  #string to jsonobj
-	    #print(type(jsonobj))
-	    #print(jsonobj)
-
-	    handleclass.writebackcfgini(PROJECT_ROOT+ '/hifcfg.ini',jsonobj)
-            '''
-	    self.handleclass =  HandleCfgFile()
-            jsonStr = handleclass.tojsondata(PROJECT_ROOT+ '/hifcfg.ini')
-            self.wfile.write(jsonStr)
-            '''
-            return 0         
-	
 #       self.send_header("Content-Length", str(len(content)))  
  
        self.wfile.write('Client: %sn ' % str(self.client_address) )
@@ -286,11 +251,63 @@ class ThreadingHTTPServer(ThreadingMixIn,HTTPServer):
 
 #启动服务函数
 def start_server(port):
-     print('[LIRO-DEBUG] enter start_server')
+     handleclass =  HandleCfgFile()
+    
 
+     hostname = socket.gethostname()
+     ip = socket.gethostbyname(hostname)
+
+     #write server.ini file
+     jsonStr = handleclass.tojsondata(PROJECT_ROOT+ '/webserver/cfg/server.ini',*{'ARG'})
+     
+     jsondata = json.loads(jsonStr)
+
+     jsondata[0]['ip'] = ip    #update ip value
+     jsondata[0]['port'] = port #update port value
+     jsondata[0]['hostname'] = hostname #update hostname
+     
+     handleclass.writebackcfgini(PROJECT_ROOT+ '/webserver/cfg/server.ini',jsondata)
+
+     #write server.json
+     filepath = PROJECT_ROOT + '/webserver/cfg/server.json'
+     print filepath
+
+
+
+     jsondata =""
+     with open(filepath,'r') as file:
+        jsondata = json.load(file)
+        print(jsondata)
+        jsondata['ip'] = ip
+        jsondata['port'] = port
+        jsondata['hostname'] = hostname
+     
+     with open(filepath ,'w') as file:
+        json.dump(jsondata,file)
+
+
+
+     print('[LIRO-DEBUG] enter start_server: hostname= [%s], ip= [%s], port= [%s] ' % (hostname,ip,port))
      http_server = ThreadingHTTPServer(('', int(port)),TestHTTPHandler)
      http_server.serve_forever() #设置一直监听并接收请求
      sys.exit(0)
- 
+
+
+def getPort():
+    pscmd = "netstat -ntl |grep -v Active|grep -v Proto|awk '{print $4}' |awk -F: '{print $NF}'"
+    procs = os.popen(pscmd).read()
+    procarr = procs.split("\n")
+    tt = random.randint(15000,20000)
+    if tt not in procarr:
+        print(tt)
+        return tt
+    else:
+        return getPort()
+def close_server():
+    print("close web server !!!")
+
+    os._exit(0)
+
 os.chdir('static') #改变工作目录到 static 目录
-start_server(8888) #启动服务，监听8000端口
+randomport = getPort()
+start_server(randomport) #启动服务，监听8000端口
